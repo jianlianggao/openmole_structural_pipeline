2f3835314c3ebd7d4b6b41cd1ddc6bc9a49b0c35
Jianliang Gao <jgao@zuli.doc.ic.ac.uk>
2018-05-14 15:41:52 +0100

    added comments and fixed a bug of over expanding array of subject ID

--- matrix_5.py
+++ matrix_5.py
@@ -1,3 +1,6 @@
+## This Python code works only with data path in 
+## /vol/dhcp-derived-data/derived_v1.1_github/ReconstructionsRelease03/derivatives
+## to find T1w.nii.gz and T2w.nii.gz files.
 import sys, os.path
 import numpy
 from numpy import *
@@ -6,6 +9,10 @@ import csv
 
 input = open(sys.argv[1],'r')
 #n = float(sys.argv[2])
+#debug
+#print(sys.argv[1])
+#print(sys.argv[2])
+#print(sys.argv[3])
 
 #print("reading the matrix")
 data = csv.reader(input, delimiter="\t")
@@ -16,13 +23,27 @@ array = numpy.array(list(data)).astype(str)
 array = array[:,[0,2]]
 
 array1 = numpy.array([[sys.argv[2]]]) #add subject ID
+array1tmp = array1
 
+# To expand subject ID array to match the array of
+# session ID and scan ages from the .tsv file
+# by replicating the rows of subject ID
 for ii in range(array1.shape[0], array.shape[0]):
-  array1 = numpy.append(array1, array1, axis = 0)
-  
+  array1 = numpy.append(array1, array1tmp, axis = 0)
+
+print("====")
+print(array)
+print(array.shape)
+print("subject ID matrix expanded:")  
+print(array1)
+print(array1.shape)
+
 array = numpy.append(array1, array, axis = 1)
 
-print(array)
+#print(array)
+#print(array.shape)
+#print(array1)
+#print(array1.shape)
 #print(n)
 #mult = array * n
 
@@ -46,6 +67,7 @@ for ii in range(0, array.shape[0]):
 #print("saving the matrix")
 #numpy.savetxt(sys.argv[3], mult, fmt='%g')
 # save to  file
+
 title_str =  numpy.array([["subject_ID", "session_ID", "scan_age", "T1_para", "T2_para"]])
 
 if (not os.path.exists(sys.argv[3])):
